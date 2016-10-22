7cf2f74cbd93fce129cce5733e8f80962db3f606
alexmorten <alexandrius.martinus@gmx.de>
2016-10-22 15:26:58 +0200

    fixed typo

--- app/models/location.js
+++ app/models/location.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   lng: DS.attr('number'),
   lat: DS.attr('number'),
-  post: DS.belongsTo('post'{ async: false })
+  post: DS.belongsTo('post',{ async: false })
 });
