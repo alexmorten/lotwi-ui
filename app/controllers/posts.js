0b25527b88387e03a1b6ca5cdce15696be6348e4
alexmorten <alexandrius.martinus@gmx.de>
2017-01-01 23:52:27 +0100

    integrated big map into posts route and got rid of locations route

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -1,15 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  queryParams: ['limit','radius','query','title','text'],
+  queryParams: ['limit','radius','query','title','text','mode'],
   limit: 10,
   radius:5,
   query:'',
   title:null,
   text:null,
+  mode:'list',
+  zoom:13,
   currentTime:new Date(),
   sortBy:["created:desc"],
   posts:Ember.computed.sort("model.posts","sortBy"),
+  showingMap:function(){
+    return this.get("mode") === "map";
+  }.property("mode"),
+  radiusMeters:function(){
+    return this.get("radius")*1000;
+  }.property("radius"),
   init(){
     var that=this;
     var update = function(){
@@ -24,5 +32,14 @@ export default Ember.Controller.extend({
   willDestroy(){
     this.set("currentTime",null);
   },
-
+  actions:{
+    toggleMode(){
+      var mode=this.get("mode");
+      if(mode==="list"){
+        this.set("mode","map");
+      }else{
+        this.set("mode","list");
+      }
+    }
+  }
 });
--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('posts',{path:"/"});
-  this.route('locations');
+  
 });
 
 export default Router;
